f77f9f0fb8eb2076efdf46b2e901347907726739
Taitranz <taitranz@ymail.com>
2025-02-23 00:40:58 +1000

    Added correct and incorrect style for btn__top, btn__bottom, btn__middle, and smoothed animation of btn__top y translate

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,18 +1,14 @@
 function toggleActiveState(el) {
-    const container = el.parentElement;
-    let button;
-    // If the clicked element is btn__middle, use the btn__top element for styling
-    if (el.classList.contains("btn__middle")) {
-        button = container.querySelector(".btn__top");
-    } else {
-        button = el;
-    }
+    // Find the container whether clicked on container or any child element
+    const container = el.classList.contains("btn-toggle--select-pairs") ? el : el.closest(".btn-toggle--select-pairs");
+    if (!container) return;
 
+    const button = container.querySelector(".btn__top--select-pairs");
     container.classList.toggle("active");
 
     button.classList.remove("border-changed");
     const btnBottom = container.querySelector(".btn__bottom--select-pairs");
-    const btnMiddle = container.querySelector(".btn__middle");
+    const btnMiddle = container.querySelector(".btn__middle--select-pairs");
 
     if (btnBottom) {
         btnBottom.classList.remove("border-changed");
